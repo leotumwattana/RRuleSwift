e68dae9dc018662add34b9bc25bc43700a8a5778
Harry Ng <harryworld@gmail.com>
2023-11-08 19:42:57 +0800

    Use process

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "RRuleSwift",
-            resources: [.copy("Resources")]
+            resources: [.process("Resources")]
         ),
         .testTarget(
             name: "RRuleSwiftTests",
